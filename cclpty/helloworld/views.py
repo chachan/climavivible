5413f87db53d32f50127729e9d3714b95871f630
lzambrano18 <lezy17@gmail.com>
2017-01-09 13:04:33 -0400

    (master) Project in django with env standard and ndb datastore

---
+++ cclpty/helloworld/views.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from django.http import HttpResponse
+from helloworld.models import Greeting
+
+
+def index(request):
+    content = Greeting(content='Sandy')
+    content.put()
+    return HttpResponse(
+        'Hello, World. This is Django running on Google App Engine')
